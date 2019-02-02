a8532bf2a271c4d18d94af2f830e635272215ed7
rasioatmaja29 <rasioatmaja29@gmail.com>
2019-02-02 09:49:46 +0700

    add to searc multiple keywords

--- web_scraping.py
+++ web_scraping.py
@@ -5,30 +5,30 @@ import time
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-def tv_series_search():
-        http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
-        request = http.request('GET', 'https://qxr.pw/hashes/')
-        html = request.data.decode('utf-8')
 
-        soup = BeautifulSoup(html, "lxml")
-        list_product = soup.find_all('a')
+def movies_search():
+    http = urllib3.PoolManager(cert_reqs="CERT_REQUIRED", ca_certs=certifi.where())
+    request = http.request("GET", "https://qxr.pw/hashes/")
+    html = request.data.decode("utf-8")
 
-        isMatchFound = False
-        keywords = ["person", "of", "interest"]
-        #for p in list_product:
-        for p in list_product[-10:]:
-        #if p.text.lower().find("person") != -1:
-                if all(keyword in p.text.lower() for keyword in keywords):
-                        print(p.text)
-                        isMatchFound = True
-                        #os.startfile(p['href'])
+    soup = BeautifulSoup(html, "lxml")
+    list_movie = soup.find_all("a")
 
-        if not isMatchFound:
-                print (str(datetime.now()),"keyword not found")
+    isMatchFound = False
+    keyword_list = [["person", "of", "interest"], ["aquaman", "2018"], ["bumblebee", "2018"]]
+    for p in list_movie[-10:]:
+        for keywords in keyword_list:
+            if all(keyword in p.text.lower() for keyword in keywords):
+                print(str(datetime.now()), p.text)
+                isMatchFound = True
+                # os.startfile(p['href'])
 
-schedule.every(5).seconds.do(tv_series_search)
+    if not isMatchFound:
+        print(str(datetime.now()), "keyword not found")
+
+
+schedule.every(5).seconds.do(movies_search)
 while 1:
-        schedule.run_pending()
-        time.sleep(1)
-
+    schedule.run_pending()
+    time.sleep(1)
 
