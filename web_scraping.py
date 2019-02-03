4a87a43742c9e93c96e367b1e084b3f4519e8334
rasioatmaja29 <rasioatmaja29@gmail.com>
2019-02-03 22:36:20 +0700

    when keywor found the delete keyword from list

--- web_scraping.py
+++ web_scraping.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 
 
+keyword_list = [["person", "of", "interest"], ["aquaman", "2018"], ["bumblebee", "2018"], ["gravity"],["cloverfield"]]
 def movies_search():
     http = urllib3.PoolManager(cert_reqs="CERT_REQUIRED", ca_certs=certifi.where())
     request = http.request("GET", "https://qxr.pw/hashes/")
@@ -15,19 +16,20 @@ def movies_search():
     list_movie = soup.find_all("a")
 
     isMatchFound = False
-    keyword_list = [["person", "of", "interest"], ["aquaman", "2018"], ["bumblebee", "2018"]]
-    for p in list_movie[-10:]:
+    
+    for p in list_movie[-50:]:
         for keywords in keyword_list:
             if all(keyword in p.text.lower() for keyword in keywords):
                 print(str(datetime.now()), p.text)
                 isMatchFound = True
-                # os.startfile(p['href'])
+                keyword_list.remove(keywords)
+                #os.startfile(p['href'])
 
     if not isMatchFound:
         print(str(datetime.now()), "keyword not found")
     
-#schedule.every(5).seconds.do(movies_search)
-schedule.every().hour.do(movies_search)
+schedule.every(5).seconds.do(movies_search)
+#schedule.every().hour.do(movies_search)
 while 1:
     schedule.run_pending()
     time.sleep(1)
